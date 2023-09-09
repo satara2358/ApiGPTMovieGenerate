9c8a29231e4988fca5058444d0758638d9f52e37
Raúl Peña <25549072+satara2358@users.noreply.github.com>
2023-09-09 18:55:08 -0500

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const process = {
     env: {
-        OPENAI_API_KEY: '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'
+        OPENAI_API_KEY:'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'
     }
 }
